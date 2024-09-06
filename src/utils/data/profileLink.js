b9e8f5d3b949f22f28b1f8a400d05027813794a6
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2024-09-06 09:10:02 +0600

    refactor: data separated from components

---
+++ src/utils/data/profileLink.js
@@ -0,0 +1,3 @@
+const profileLink = "https://w7.pngwing.com/pngs/1000/665/png-transparent-computer-icons-profile-s-free-angle-sphere-profile-cliparts-free-thumbnail.png";
+
+export default profileLink;
