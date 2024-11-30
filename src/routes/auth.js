00b78315407c821e58656d5cdb328ae72c64fb81
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2024-11-30 11:49:51 +0600

    chore: route added for test-mail, forgot-password, verify-reset-otp

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,8 +6,15 @@ const {
   // logoutAlumni,
   // checkTokenValidity,
   // refreshToken,
+  testEmail,
+  forgotPassword,
+  verifyResetOtp,
+  resetPassword,
 } = require('../controllers/authController');
 const { validateToken } = require('../middleware/validateToken');
+const { validateEmailRequest } = require('../middleware/validateEmail');
+const { emailLimiter, otpLimiter } = require('../middleware/rateLimiter');
+const { isAdmin } = require('../middleware/validateRole');
 
 // Set up multer storage configuration
 const storage = multer.diskStorage({
@@ -36,5 +43,25 @@ router.post('/login', login);
 
 // // Refresh Token
 // router.post('/refresh-token', refreshToken);
+
+// test email
+// @route POST /test-email
+// @access Public
+router.post('/test-email',isAdmin, testEmail);
+
+// forgot password
+// @route POST /forgot-password
+// @access Public
+router.post("/forgot-password",emailLimiter, otpLimiter, validateEmailRequest , forgotPassword);
+
+// verify reset otp
+// @route POST /verify-reset-otp
+// @access Public
+router.post("/verify-reset-otp", emailLimiter, otpLimiter, validateEmailRequest, verifyResetOtp);
+
+// reset password
+// @route POST /reset-password
+// @access Public
+router.post("/reset-password",emailLimiter, otpLimiter, validateEmailRequest, resetPassword); 
 
 module.exports = router;
